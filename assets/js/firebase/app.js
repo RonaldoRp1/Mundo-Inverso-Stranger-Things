021c14445c8b6a110a7278b6829ee8122bd2a571
Perkles <kariaodlanor@gmail.com>
2022-08-26 02:54:03 -0300

    Mundo inverso

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+    import {initializeApp} from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyDtiMOKLrgZ3d0MWgZR1ceyjA7RJfmDCRA",
+    authDomain: "projeto-teste-4a408.firebaseapp.com",
+    projectId: "projeto-teste-4a408",
+    storageBucket: "projeto-teste-4a408.appspot.com",
+    messagingSenderId: "1003441064505",
+    appId: "1:1003441064505:web:96c7a5f54650531dfe1be8"
+  };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    export default app
